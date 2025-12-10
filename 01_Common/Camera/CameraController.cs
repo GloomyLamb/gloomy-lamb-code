9d19535f882064eaf7150074d44a5efe13fd74b1
sooroora <sura6856@naver.com>
2025-12-11 00:10:12 +0900

    feat: 기본 카메라 조작 추가
    - 카메라 조작을 플레이어와 분리해서 구현
    - 비춰줄 Target을 설정할 수 있게 구현

--- 01_Common/Camera/CameraController.cs
+++ 01_Common/Camera/CameraController.cs
@@ -1,22 +1,24 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using Cinemachine;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using UnityEngine.UIElements;
 
-// TpsCameraController 등으로 바꿔야할 것 같음
-// abstract 하나 만들어야할듯!
+
 public class CameraController : MonoBehaviour
 {
-    // todo : 카메라 매니저 들어오면 수정하기 
-    [SerializeField] private InputActionAsset inputAction;
-    [SerializeField] private float lookSensitivity = 5;
-    protected InputHandler input;
+    // todo : 카메라 매니저와 논의 필요
+    // 임시 public! 나중에는 SetTarget 으로 타겟팅해주기
+    public Transform target;
     
-    CinemachineVirtualCamera virtualCamera;
-    private CinemachineOrbitalTransposer orbital;
+    [Header("Input")]
+    [ SerializeField ] private InputActionAsset inputAction;
+
+    [ Header( "설정" ) ]
+    [ SerializeField ] private CinemachineVirtualCamera vCam;
+    [ SerializeField ] private Transform pivot;
+    [ SerializeField ] private float lookSensitivity = 20;
+    [ SerializeField ] private float limitMinX = 0;
+    [ SerializeField ] private float limitMaxX = 30;
+    protected InputHandler input;
 
     private float camCurRotX;
     private float camCurRotY;
@@ -28,31 +30,44 @@ public class CameraController : MonoBehaviour
 
     public virtual void Init()
     {
-        virtualCamera = GetComponentInChildren<CinemachineVirtualCamera>();
-        orbital = virtualCamera.GetCinemachineComponent<CinemachineOrbitalTransposer>();
+        input = new InputHandler( inputAction, InputType.Camera );
 
-        input = new InputHandler(inputAction, InputType.Camera);
+        camCurRotX = transform.eulerAngles.x;
+        camCurRotY = transform.eulerAngles.y;
+        
+        // todo : CameraManager 에 등록하기
+    }
+
+    public void UseInput()
+    {
+        // todo : CameraManager 한테 쓴다고 쪼르기
+    }
+
+    public void SetTarget( Transform _target )
+    {
+        target = _target;
+        camCurRotY = target.eulerAngles.y;
     }
 
     private void Start()
     {
-        
+        InputManager.Instance.UseInput( input );
+        InputManager.Instance.HideCursor();
     }
 
     private void Update()
     {
-
-        Vector2 axis = input.GetAxis(InputMapName.Default, InputActionName.Look);
-        
-        camCurRotX += (axis.y * Time.deltaTime * lookSensitivity);
-        
-        camCurRotY += (axis.x * Time.deltaTime* lookSensitivity);
-
-        //Debug.Log(camCurRotX);
-        orbital.m_Heading.m_Bias = camCurRotY;
-
     }
-    
-    
 
+    private void LateUpdate()
+    {
+        pivot.position = target.position;
+        Vector2 axis = input.GetAxis( InputMapName.Default, InputActionName.Look );
+        
+        camCurRotX += ( -axis.y * Time.deltaTime * lookSensitivity );
+        camCurRotX = Mathf.Clamp( camCurRotX, limitMinX, limitMaxX );
+        camCurRotY += ( axis.x * Time.deltaTime * lookSensitivity );
+
+        pivot.rotation = Quaternion.Euler( camCurRotX, camCurRotY, 0 );
+    }
 }
