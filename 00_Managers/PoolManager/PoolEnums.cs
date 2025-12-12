23b5f3a6b71628e300ac9f53ebb31418f12e7ba0
SuraJeon <sura6856@naver.com>
2025-12-12 23:37:38 +0900

    feat: 기존 PoolManager import

---
+++ 00_Managers/PoolManager/PoolEnums.cs
@@ -0,0 +1,9 @@
+﻿/// <summary>
+/// 풀링해서 쓸 오브젝트 타입들 추가 
+/// </summary>
+public enum PoolType
+{
+    // 예시
+    Monster,
+    Bullet,
+}
---
+++ 00_Managers/PoolManager/PoolManager.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class PoolManager : SceneSingletonManager<PoolManager>
+{
+    [SerializeField] List<BasePool> pools;
+    Dictionary<PoolType, BasePool> poolDic;
+
+    protected override void Init()
+    {
+        poolDic = ((PoolType[])Enum.GetValues(typeof(PoolType))).ToDictionary(part => part,
+            part => (BasePool)null);
+
+        foreach (var pool in pools)
+        {
+            if (Enum.TryParse(pool.gameObject.name, ignoreCase: true, out PoolType poolType))
+            {
+                pool.Init();
+                poolDic[poolType] = pool;
+            }
+        }
+    }
+
+    public GameObject Spawn(PoolType poolType, Vector3 position, Quaternion rotation, Transform parent = null)
+    {
+        if (poolDic.ContainsKey(poolType))
+        {
+            GameObject newGameObject = poolDic[poolType].GetGameObject();
+            
+            newGameObject.transform.position = position;
+            newGameObject.transform.rotation = rotation;
+            
+            
+            if (parent != null)
+            {
+                newGameObject.transform.SetParent(parent);
+            }
+            
+            return newGameObject;
+        }
+
+        return null;
+    }
+
+    public GameObject Spawn(PoolType poolType)
+    {
+        if (poolDic.ContainsKey(poolType))
+        {
+            GameObject newGameObject = poolDic[poolType].GetGameObject();
+            return newGameObject;
+        }
+        return null;
+    }
+
+    public void DeactivateAllPoolObjects(PoolType poolType)
+    {
+        if (poolDic.ContainsKey(poolType))
+        {
+            poolDic[poolType].DeactivateAllPoolObjects();
+        }
+    }
+}
