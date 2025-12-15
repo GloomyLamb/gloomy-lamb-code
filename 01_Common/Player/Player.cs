53a6f8125506ac7ca38eeb560c3039383a36f703
shin0112 <wnslcosltimo12@gmail.com>
2025-12-15 11:09:10 +0900

    feat: 상호작용에 필요한 로직 추가하기
    - 오브젝트 탐색
    - 우선순위 계산
    - 타겟 확정

--- 01_Common/Player/Player.cs
+++ 01_Common/Player/Player.cs
@@ -1,4 +1,3 @@
-using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -9,25 +8,94 @@ public abstract class Player : MonoBehaviour
     [Header("Input")]
     [SerializeField] protected InputActionAsset inputAction;
     protected InputHandler input;
-    
+
     [Header("Pivot")]
     [SerializeField] protected Transform pivot;
-    
+
+    [Header("Interaction Setting")]
+    [SerializeField] protected float interactRange = 3f;
+    [SerializeField] protected float interactAngle = 60f;
+    [SerializeField] protected LayerMask interactableLayer;
+
+    // 가중치
+    [SerializeField] protected bool angleWeight = false;
+    [SerializeField] protected bool distanceWieght = true;
+    private float cosThreshold;
+
     public Vector3 Forward => forward;
     protected Vector3 forward;
-    
+
     private void Awake()
     {
         input = new InputHandler(inputAction, InputType.Player);
         forward = transform.forward;
+        cosThreshold = Mathf.Cos(Mathf.Deg2Rad * (interactAngle / 2));
         Init();
     }
 
     protected abstract void Init();
-    
+
     private void OnDestroy()
     {
         input?.DisposeInputEvent();
     }
-    
+
+    #region 상호작용
+    protected void OnDetectInteractablesEnter()
+    {
+        Collider[] cols = DetectInteractable();                     // 1. 반경 내 오브젝트 탐색
+        IInteractable interactable = FindBestInteractable(cols);    // 2. 우선순위 계산 (각도 + 거리)
+        interactable?.PopUpKey();                                   // 3. 타겟 확정 -> 팝업 표시
+    }
+
+    /// <summary>
+    /// 반경 내 오브젝트 탐색
+    /// </summary>
+    protected virtual Collider[] DetectInteractable()
+    {
+        return Physics.OverlapSphere(
+           transform.position,
+           interactRange,
+           interactableLayer);
+    }
+
+    /// <summary>
+    /// 우선 순위 계산 (각도 + 거리)
+    /// </summary>
+    /// <param name="cols"></param>
+    /// <returns></returns>
+    protected virtual IInteractable FindBestInteractable(Collider[] cols)
+    {
+        IInteractable best = null;
+        float bestScore = float.MaxValue;
+
+        // 현재 기준: 거리
+        foreach (var col in cols)
+        {
+            if (!col.TryGetComponent<IInteractable>(out var interactable)) continue;
+
+            // 거리 계산
+            Vector3 dir = col.transform.position - transform.position;
+            float sqrDistance = dir.sqrMagnitude;
+            if (sqrDistance < 0.0001f) continue;
+
+            // 각도 계산
+            float dot = Vector3.Dot(forward, dir) / Mathf.Sqrt(sqrDistance);
+            if (dot < cosThreshold) continue;
+
+            // 각도 + 거리 가중치 합산 (점수가 낮을수록 우선순위 높음)
+            float score =
+                (1f - dot) * (angleWeight ? 1 : 0)
+                + sqrDistance * (distanceWieght ? 1 : 0);
+
+            if (score < bestScore)
+            {
+                bestScore = score;
+                best = interactable;
+            }
+        }
+
+        return best;
+    }
+    #endregion
 }
