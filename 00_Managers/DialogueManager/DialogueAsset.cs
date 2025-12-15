2b3294c10541944d5257b53c0fa0092c74b93490
sooroora <sura6856@naver.com>
2025-12-15 17:37:41 +0900

    feat: DialogueManager 선언, DialogueData 구조 추가

---
+++ 00_Managers/DialogueManager/DialogueAsset.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New DialogueAsset", menuName = "SO/Dialogue Asset")]
+public class DialogueAsset : ScriptableObject
+{
+    [SerializeField] private TextAsset dialogueOrigin;
+    [SerializeField] private List<DialogueData> dialogueData;
+
+    void Parse()
+    {
+    }
+
+#if UNITY_EDITOR
+    private void OnValidate()
+    {
+        if (dialogueOrigin == null) return;
+        Parse();
+    }
+#endif
+}
---
+++ 00_Managers/DialogueManager/DialogueData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class DialogueData
+{
+    [SerializeField] private string name;
+    [SerializeField] private string dialogue;
+    [SerializeField] private string sprName;
+    [SerializeField] private DialogueEmotionType emotion;
+    [SerializeField] List<DialogueButtonData> buttons;
+}
