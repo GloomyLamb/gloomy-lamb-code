c8c40fc96ac80fed689c1e80adbbee6660f67dcc
shin0112 <wnslcosltimo12@gmail.com>
2025-12-15 11:08:49 +0900

    feat: 상호작용 오브젝트용 인터페이스 작성

---
+++ 01_Common/IInteractable.cs
@@ -0,0 +1,19 @@
+/// <summary>
+/// 상호작용 인터페이스
+/// </summary>
+public interface IInteractable
+{
+    /// <summary>
+    /// 상호작용 내부 로직
+    /// </summary>
+    public void Interact();
+
+    /// <summary>
+    /// 상호작용 키 팝업
+    /// </summary>
+    public virtual void PopUpKey()
+    {
+        // todo: 일반 키 넣고 필요 시 오버라이드
+        Logger.Log("상호작용 키 팝업");
+    }
+}
