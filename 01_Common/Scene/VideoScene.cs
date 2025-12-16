ac4ef34d5f5f7ba457a9fa84fee84ae7eb0af435
shin0112 <wnslcosltimo12@gmail.com>
2025-12-16 17:54:43 +0900

    feat: 비디오 씬 클래스 작성하기
    - 씬 전환 시 호출되는 sceneLoaded용 함수
    - 비디오 재생 종료 시 실행할 메서드

---
+++ 01_Common/Scene/VideoScene.cs
@@ -0,0 +1,44 @@
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// 비디오 씬 클래스
+/// </summary>
+public class VideoScene : SceneBase
+{
+    private VideoController _videoController;
+
+    /// <summary>
+    /// 씬 전환 시 호출되는 메서드
+    /// 게임 매니저에 저장되어 있는 video flow context를 참조하여 비디오를 재생하고,
+    /// 비디오 재생이 종료되었을 때의 이벤트를 구독한다.
+    /// </summary>
+    /// <param name="scene"></param>
+    /// <param name="mode"></param>
+    public override void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        base.OnSceneLoaded(scene, mode);
+
+        _videoController = FindObjectOfType<VideoController>();
+
+        _videoController.OnVideoFinished += HandleVideoFinished;
+        VideoFlowContext videoFlowContext = GameManager.Instance.VideoFlow;
+        _videoController.Init(
+            videoFlowContext.VideoID,
+            videoFlowContext.ReturnScene.ToString());
+    }
+
+    /// <summary>
+    /// 비디오 재생이 종료되었을 때 호출되는 메서드
+    /// video flow context에서 반환할 씬을 가져와 해당 씬으로 전환한다.
+    /// </summary>
+    private void HandleVideoFinished()
+    {
+        _videoController.OnVideoFinished -= HandleVideoFinished;
+
+        VideoFlowContext videoFlowContext = GameManager.Instance.VideoFlow;
+        SceneType next = videoFlowContext.ReturnScene;
+        videoFlowContext.Clear();
+
+        GameManager.Instance.Scene.LoadSceneWithCoroutine(next);
+    }
+}
