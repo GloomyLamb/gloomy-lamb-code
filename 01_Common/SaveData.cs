56184265856d5404e3370d07be090cf4f011d068
곽호빈 <hobin1117@gmail.com>
2025-12-16 16:55:01 +0900

    feat:저장시스템 구현
    저장시스템 기능 추가했습니다

---
+++ 01_Common/SaveData.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+[Serializable]
+public class SaveData
+{
+    public string SceneName;
+    public Vector3 PlayerPosition;
+    public int ClearChapterNumber;
+    public string PlayerPositionX;
+    public string PlayerPositionY;
+    public string PlayerPositionZ;
+
+
+}
