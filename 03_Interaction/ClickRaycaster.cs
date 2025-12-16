57e56f9bb4ab49c0ee53ec0f096044100adecfa0
shin0112 <wnslcosltimo12@gmail.com>
2025-12-16 11:13:55 +0900

    feat: Ray 쏴서 npc 위치 확인하기

--- 03_Interaction/ClickRaycaster.cs
+++ 03_Interaction/ClickRaycaster.cs
@@ -20,19 +20,37 @@ public class ClickRaycaster : MonoBehaviour
         Init();
         _camera = Camera.main;
     }
+
     private void Init()
     {
         _input = new InputHandler(_inputActions, InputType.Camera);
         _input.BindInputEvent(InputMapName.Default, InputActionName.Interaction, OnClick);
     }
 
+    private void Update()
+    {
+        //Ray ray = _camera.ScreenPointToRay(Input.mousePosition);
+        //Debug.DrawRay(ray.origin, ray.direction * _maxDistance, Color.red, 1f);
+    }
+
     private void OnClick(InputAction.CallbackContext context)
     {
-        Ray ray = _camera.ScreenPointToRay(Input.mousePosition);
-
-        if (Physics.Raycast(ray, out RaycastHit hit, _maxDistance, _layerMask))
+        if (context.phase == InputActionPhase.Performed)
         {
+            Logger.Log("마우스 클릭");
+            if (_camera == null) return;
 
+            Vector2 mousePos = Mouse.current.position.ReadValue();
+            Ray ray = _camera.ScreenPointToRay(mousePos);
+            Debug.DrawRay(ray.origin, ray.direction * _maxDistance, Color.red, 1f);
+
+            if (Physics.Raycast(ray, out RaycastHit hit, _maxDistance, _layerMask))
+            {
+                if (hit.collider.TryGetComponent<NPCBase>(out var npc))
+                {
+                    npc.Interact();
+                }
+            }
         }
     }
 }
