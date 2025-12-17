273f244b320f1bb0f37109fd060d1908e6f39a96
shin0112 <wnslcosltimo12@gmail.com>
2025-12-17 22:02:18 +0900

    feat: 스킬 상태 데이터 SO 만들기

---
+++ 05_Skill/SkillEnums.cs
@@ -0,0 +1,11 @@
+public enum SkillType
+{
+    // Normal
+    Binding,
+    HeadButt,
+
+    // Lighting
+    Beam,
+
+    Test = 99,
+}
---
+++ 05_Skill/SkillStatusData.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "new SkillStatusData", menuName = "SO/Skill Status Data")]
+public class SkillStatusData : ScriptableObject
+{
+    [field: SerializeField] public SkillType SkillType { get; private set; }
+    [field: SerializeField] public float Cooldown { get; private set; }
+    [field: SerializeField] public float AttackDamage { get; private set; }
+    [field: SerializeField] public float Knockback { get; private set; }
+    [field: SerializeField] public AttackRange AttackRange { get; private set; }
+    [field: SerializeField] public Duration Duration { get; private set; }
+}
+
+[System.Serializable]
+public class AttackRange
+{
+    // 임의로 넣은 값이라 더 회의 필요
+    [field: SerializeField] public float Radius { get; private set; }
+}
+
+[System.Serializable]
+public class Duration
+{
+    [field: SerializeField] public float TotalTime { get; private set; }
+    [field: SerializeField] public float AttackPoint { get; private set; }
+    [field: SerializeField] public float AttackTime { get; private set; }
+}
