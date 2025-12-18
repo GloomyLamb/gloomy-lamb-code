809231f14df20c5aa292b8e696186f80a37735d8
shin0112 <wnslcosltimo12@gmail.com>
2025-12-18 09:25:44 +0900

    refactor: 상호작용 - 입력 로직 수정하기

--- 01_Common/Player/Player.cs
+++ 01_Common/Player/Player.cs
@@ -1,5 +1,4 @@
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 // todo : Player 구조 다같이 이야기 해보기
 // controller 로 빼도 되지만, 우리 Player 들이 생각보다 가벼울 것임
@@ -25,7 +24,6 @@ public abstract class Player : MonoBehaviour
 
     protected virtual void Update()
     {
-        interaction?.UpdateInteractionTarget();
     }
 
     private void OnDestroy()
