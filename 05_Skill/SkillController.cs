96a80e8053c6d068f5388e014d42a1e842091147
shin0112 <wnslcosltimo12@gmail.com>
2025-12-18 10:06:37 +0900

    refactor: 스킬 - 인풋 매니저 사용해서 인풋과 스킬 이벤트 연결하기

--- 05_Skill/SkillController.cs
+++ 05_Skill/SkillController.cs
@@ -2,12 +2,15 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 
+/// <summary>
+/// 스킬을 저장하고 관리하는 컨트롤러
+/// </summary>
 public class SkillController : MonoBehaviour
 {
     [SerializeField] private SkillStatusDatabase _skillStatusDatabase;
 
-    private Dictionary<SkillType, BaseSkill> _skillDict = new();
-
+    // 스킬 상태 관리
+    private readonly Dictionary<SkillType, BaseSkill> _skillDict = new();
     private BaseSkill _curSkill = null;
 
     /// <summary>
@@ -40,6 +43,31 @@ public class SkillController : MonoBehaviour
     }
 
     /// <summary>
+    /// [public] 스킬과 input 묶기
+    /// </summary>
+    /// <param name="type"></param>
+    /// <param name="inputType"></param>
+    /// <param name="inputMapName"></param>
+    /// <param name="inputActionName"></param>
+    public void BindInput(
+        SkillType type,
+        InputType inputType,
+        InputMapName inputMapName,
+        InputActionName inputActionName
+        )
+    {
+        if (!_skillDict.TryGetValue(type, out BaseSkill skill))
+        {
+            Logger.LogWarning($"{skill} 없음");
+            return;
+        }
+
+        InputManager.Instance.BindInputEvent(inputType, inputMapName, inputActionName, skill.OnUseSkill);
+        InputManager.Instance.UseInput(inputType);
+    }
+
+    #region curSkill 관리
+    /// <summary>
     /// 스킬 사용 시 curSkill에 값 저장
     /// </summary>
     /// <param name="baseSkill"></param>
@@ -61,6 +89,7 @@ public class SkillController : MonoBehaviour
     {
         _curSkill = null;
     }
+    #endregion
 
     #region 에디터 전용
 #if UNITY_EDITOR
