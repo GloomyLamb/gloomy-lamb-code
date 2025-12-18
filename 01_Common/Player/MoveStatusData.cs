4c613e387e0d616e1cdb549668d8eeb8b61c8ba9
SuraJeon <sura6856@naver.com>
2025-12-18 10:42:43 +0900

    feat: MoveStatusData ScriptableObject추가

---
+++ 01_Common/Player/MoveStatusData.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "New MoveStatusData", menuName = "SO/Move Status Data")]
+public class MoveStatusData : ScriptableObject
+{
+    public float MoveSpeed => moveSpeed;
+    public float DashSpeed => dashSpeed;
+    public float JumpForce => jumpForce;
+    public float JumpDelayTime => jumpDelayTime;
+    
+    
+    [Header("이동 속도")]
+    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float dashSpeed = 7.5f;  // 지정 속도로 하는게 편하겠죠?
+    
+    [Header("점프 설정")]
+    [SerializeField] private float jumpForce = 5f;
+    [SerializeField] private float jumpDelayTime = 0.2f;
+    
+    
+}
