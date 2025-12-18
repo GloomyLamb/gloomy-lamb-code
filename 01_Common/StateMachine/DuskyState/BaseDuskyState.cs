a5770d6724f9e258c1ff4f5abb800b82206ca810
shin0112 <wnslcosltimo12@gmail.com>
2025-12-18 15:13:46 +0900

    feat: 베이스 클래스 - 더스키, 그림자 base state 클래스 만들기

---
+++ 01_Common/StateMachine/DuskyState/BaseDuskyState.cs
@@ -0,0 +1,9 @@
+/// <summary>
+/// 더스키 상태 베이스 클래스
+/// </summary>
+public class BaseDuskyState : BaseMoveableState
+{
+    public BaseDuskyState(MoveableStateMachine stateMachine) : base(stateMachine)
+    {
+    }
+}
