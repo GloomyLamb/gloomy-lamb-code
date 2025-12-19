4bd1ef4b9617edca346ff829d633eca36e2d2cd9
shin0112 <wnslcosltimo12@gmail.com>
2025-12-19 14:30:01 +0900

    move: 상호작용 스크립트 이동

--- 03_Game/03_Interaction/NPC/TestNPC.cs
+++
@@ -1,9 +0,0 @@
-public class TestNPC : BaseNPC
-{
-    public override void Interact()
-    {
-        Logger.Log("NPC와 상호작용");
-
-        DialogueManager.Instance.StartDialogue(DialogueType.Default, "안녕하세요! 저는 테스트 NPC입니다.");
-    }
-}
