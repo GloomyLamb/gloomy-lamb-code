e59023afbe8c1723570eb6cbcb4da376d89bc1a6
SuraJeon <sura6856@naver.com>
2025-12-19 20:47:55 +0900

    style: animator 파라미터 주석 추가

--- 01_Common/Define/AnimatorParameters.cs
+++ 01_Common/Define/AnimatorParameters.cs
@@ -4,11 +4,41 @@ using UnityEngine;
 
 public static class AnimatorParameters
 {
+    /// <summary>
+    /// 움직일 때 true
+    /// </summary>
     public static readonly int IsMove = Animator.StringToHash("IsMove");
-    public static readonly int IsJumping = Animator.StringToHash("IsJumping");
+    
+    /// <summary>
+    /// Attack 트리거
+    /// </summary>
     public static readonly int Attack = Animator.StringToHash("Attack");
+    
+    /// <summary>
+    /// Hit 트리거
+    /// </summary>
     public static readonly int Hit = Animator.StringToHash("Hit");
+    
+    /// <summary>
+    /// Die 트리거
+    /// </summary>
     public static readonly int Die = Animator.StringToHash("Die");
+    
+    /// <summary>
+    /// 죽은 상태일때 true
+    /// ex) DieIdle 상태에서 벗어날 때 false 처리
+    /// </summary>
+    public static readonly int IsDead = Animator.StringToHash("IsDead");
+    
+    /// <summary>
+    /// Jump 트리거
+    /// </summary>
     public static readonly int Jump = Animator.StringToHash("Jump");
+    
+    /// <summary>
+    /// 지면 위에 있지 않은 상태일때 true
+    /// </summary>
+    public static readonly int IsJumping = Animator.StringToHash("IsFalling");
+    
     
 }
