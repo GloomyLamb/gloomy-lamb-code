45f6e748dcd5eff72fd1d83a75ca65623b5930ab
SuraJeon <sura6856@naver.com>
2025-12-22 19:04:05 +0900

    move: StatusData 스크립트 Character 폴더로 이동

---
+++ 03_Game/00_Character/StatusData.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+
+public class StatusData : ScriptableObject
+{
+    [SerializeField] private int _maxHp;
+
+    public Status GetNewStatus()
+    {
+        // 지금은 hp만 있다
+        return  new Status(_maxHp);
+    }
+}
