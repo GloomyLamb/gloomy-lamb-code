3488499daf96a2296b2a4e713a376472b0bc7b3e
SuraJeon <sura6856@naver.com>
2025-12-22 16:56:14 +0900

    feat: 더스키가 사용할 State 추가

---
+++ 02_System/StateMachine/DuskyState/DuskyMoveState.cs
@@ -0,0 +1,21 @@
+﻿public class DuskyMoveState : BaseDuskyState
+{
+    public DuskyMoveState(MoveableStateMachine stateMachine) : base(stateMachine)
+    {
+    }
+
+    public override void Enter()
+    {
+        stateMachine.animator.SetBool(AnimatorParameters.IsMove, true);
+    }
+
+    public override void Update()
+    {
+        base.Update();
+    }
+    
+    public override void Exit()
+    {
+        stateMachine.animator.SetBool(AnimatorParameters.IsMove, false);
+    }
+}
