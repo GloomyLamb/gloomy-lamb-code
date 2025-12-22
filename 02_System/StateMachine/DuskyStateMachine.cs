f37683a1190256e97c0de558e092fe1e0a8a9585
SuraJeon <sura6856@naver.com>
2025-12-22 16:57:28 +0900

    feat: DuskyStateMachine 에 사용할 DuskyState 추가

--- 02_System/StateMachine/DuskyStateMachine.cs
+++ 02_System/StateMachine/DuskyStateMachine.cs
@@ -4,16 +4,29 @@ using UnityEngine;
 
 public class DuskyStateMachine : MoveableStateMachine
 {
-    private Animator _anim;
+    public BaseDuskyState IdleState => _idleState;
+    public BaseDuskyState JumpState => _jumpState;
+    public BaseDuskyState HitState => _hitState;
+    public BaseDuskyState AttackState => _attackState;
+    public BaseDuskyState DieState => _dieState;
+    public BaseDuskyState MoveState => _moveState;
+
 
     private BaseDuskyState _idleState;
     private BaseDuskyState _jumpState;
     private BaseDuskyState _hitState;
     private BaseDuskyState _attackState;
-    
+    private BaseDuskyState _dieState;
+    private BaseDuskyState _moveState;
+
+
     public DuskyStateMachine(Animator animator) : base(animator)
     {
-        _anim = animator;
+        _idleState = new DuskyIdleState(this);
+        _jumpState = new DuskyJumpState(this);
+        _hitState = new DuskyHitState(this);
+        _attackState = new DuskyAttackState(this);
+        _dieState = new DuskyDieState(this);
+        _moveState = new DuskyMoveState(this);
     }
-
 }
