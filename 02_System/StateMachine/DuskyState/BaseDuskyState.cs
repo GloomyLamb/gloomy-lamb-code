d28898d5c583e14a355d8b754c2c46108a6080e9
SuraJeon <sura6856@naver.com>
2025-12-23 11:27:30 +0900

    rename: BaseDuskyState 변수명 컨벤션에 맞추어 수정

--- 02_System/StateMachine/DuskyState/BaseDuskyState.cs
+++ 02_System/StateMachine/DuskyState/BaseDuskyState.cs
@@ -3,12 +3,13 @@ using UnityEngine.Events;
 /// <summary>
 /// 더스키 상태 베이스 클래스
 /// </summary>
-public class BaseDuskyState : BaseMoveableState
+public abstract class BaseDuskyState : BaseMoveableState
 {
-    protected DuskyPlayer _player;
+    protected DuskyPlayer player;
     
     public BaseDuskyState(MoveableStateMachine stateMachine, DuskyPlayer player) : base(stateMachine)
     {
-        _player = player;
+        this.player = player;
     }
+
 }
