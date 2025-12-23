bcf4042f0ad5d5f52eea3ef10a05f6ffc9c8b843
shin0112 <wnslcosltimo12@gmail.com>
2025-12-23 12:52:28 +0900

    feat: 그림자 개 - StateMachine에 필요한 state 만들어놓기

--- 02_System/StateMachine/Shadow/DogShadowStateMachine.cs
+++ 02_System/StateMachine/Shadow/DogShadowStateMachine.cs
@@ -5,15 +5,30 @@ public class DogShadowStateMachine : ShadowStateMachine
     public DogShadow DogShadow { get; private set; }
 
     // Ground
+    public DogShadowChaseState ChaseState { get; private set; }
+    public DogShadowIdleState IdleState { get; private set; }
+    public DogShadowTransformState TransformState { get; private set; }
 
     // Skill
+    public DogShadowBiteState BiteState { get; private set; }
+    public DogShadowBarkState BarkState { get; private set; }
 
     // Battle
+    public DogShadowHitState HitState { get; private set; }
+    public DogShadowBoundState BoundState { get; private set; }
 
     public DogShadowStateMachine(Shadow shadow, Animator animator) : base(shadow, animator)
     {
         DogShadow = shadow as DogShadow;
 
+        ChaseState = new DogShadowChaseState(this);
+        IdleState = new DogShadowIdleState(this);
+        TransformState = new DogShadowTransformState(this);
 
+        BiteState = new DogShadowBiteState(this);
+        BarkState = new DogShadowBarkState(this);
+
+        HitState = new DogShadowHitState(this);
+        BoundState = new DogShadowBoundState(this);
     }
 }
