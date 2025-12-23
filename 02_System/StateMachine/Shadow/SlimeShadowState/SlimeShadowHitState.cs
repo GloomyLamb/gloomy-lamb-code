c3d7697f4a77178bc5bef8b0fcc967aa270c8dea
shin0112 <wnslcosltimo12@gmail.com>
2025-12-23 11:08:18 +0900

    feat: 그림자 기본 - hit 애니메이션 적용하기

--- 02_System/StateMachine/Shadow/SlimeShadowState/SlimeShadowHitState.cs
+++ 02_System/StateMachine/Shadow/SlimeShadowState/SlimeShadowHitState.cs
@@ -1,11 +1,17 @@
+using UnityEngine;
+
 public class SlimeShadowHitState : SlimeShadowAttackState
 {
+    private float _timer;
+    private float _animationTime = 1f;
+
     public SlimeShadowHitState(MoveableStateMachine stateMachine) : base(stateMachine)
     {
     }
 
     public override void Enter()
     {
+        _timer = 0f;
         StateMachine.SlimeShadow.MovementSpeedModitier = 0f;
         base.Enter();
         StartAnimation(StateMachine.SlimeShadow.AnimationData.HitParameterHash);
@@ -15,5 +21,16 @@ public class SlimeShadowHitState : SlimeShadowAttackState
     {
         base.Exit();
         StopAnimation(StateMachine.SlimeShadow.AnimationData.HitParameterHash);
+    }
+
+    public override void Update()
+    {
+        base.Update();
+
+        _timer += Time.deltaTime;
+        if (_timer > _animationTime)
+        {
+            StateMachine.ChangeState(StateMachine.IdleState);
+        }
     }
 }
