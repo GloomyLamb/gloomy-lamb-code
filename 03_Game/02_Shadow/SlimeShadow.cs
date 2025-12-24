fb2db7850b506f5bb97bca680b0d983560cba2c3
SuraJeon <sura6856@naver.com>
2025-12-24 19:19:34 +0900

    feat: 슬라임 빔맞았을 때 줄어들도록 구현

--- 03_Game/02_Shadow/SlimeShadow.cs
+++ 03_Game/02_Shadow/SlimeShadow.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 /// <summary>
@@ -15,6 +16,8 @@ public class SlimeShadow : Shadow
     [field: SerializeField] public float MinScale { get; private set; } = 1f;
     public bool IsFastMode { get; set; }
 
+    [Header("추가 설정")]
+    [SerializeField] public float _scaleDownDuration = 1f;
     // 변형 조건
     private bool _checkExpand;
     private bool CheckScale => transform.localScale.x == 1f;
@@ -32,13 +35,28 @@ public class SlimeShadow : Shadow
     {
         if (collision.gameObject.TryGetComponent<IDamageable>(out var damageable))
         {
+            //todo : 
             damageable.Damage(damage);
         }
     }
 
+    public override void ApplyEffect()
+    {
+        StartScaleDownRoutine();
+    }
+
+    public override void StopEffect()
+    {
+        if (scaleDownRoutine != null)
+            StopCoroutine(scaleDownRoutine);
+    }
+
+
     #region 변형
+
     protected override bool CanTransform()
     {
+        return false;
         return _checkExpand && CheckScale;
     }
 
@@ -51,5 +69,32 @@ public class SlimeShadow : Shadow
     {
         _checkExpand = true;
     }
+
     #endregion
+
+
+    private Coroutine scaleDownRoutine;
+
+    void StartScaleDownRoutine()
+    {
+        if (scaleDownRoutine != null)
+            StopCoroutine(scaleDownRoutine);
+        scaleDownRoutine = StartCoroutine(ScaleDownRoutine(MinScale, _scaleDownDuration));
+    }
+
+    protected IEnumerator ScaleDownRoutine(float size, float duration)
+    {
+        Vector3 startScale = transform.localScale;
+        Vector3 endScale = Vector3.one * size;
+        float elapsed = 0f;
+
+        while (elapsed < duration)
+        {
+            transform.localScale = Vector3.Lerp(startScale, endScale, elapsed / duration);
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+
+        transform.localScale = endScale;
+    }
 }
