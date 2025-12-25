e44d95f128779ff23f479369cbcc63165b9f90e1
SuraJeon <sura6856@naver.com>
2025-12-25 20:21:35 +0900

    fix: 개 짖기 State 빠져나가지 못하는 문제 수정

--- 02_System/StateMachine/Shadow/DogShadowState/DogShadowBarkState.cs
+++ 02_System/StateMachine/Shadow/DogShadowState/DogShadowBarkState.cs
@@ -48,6 +48,8 @@ public class DogShadowBarkState : DogShadowSkillState
         }
         
         spawnRoutine = null;
+        
+        StateMachine.Shadow.DonePattern = true;
         StateMachine.ChangeState(StateMachine.IdleState);
     }
 }
