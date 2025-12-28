5fbcaaf26bbe340876c71826a1270aab21b6875b
shin0112 <wnslcosltimo12@gmail.com>
2025-12-28 23:54:47 +0900

    remove: 불필요한 클래스 삭제

--- 02_System/StateMachine/Shadow/SlimeShadowState/SlimeShadowState.cs
+++
@@ -1,9 +0,0 @@
-public class SlimeShadowState : ShadowState
-{
-    protected SlimeShadow SlimeShadow => (SlimeShadow)shadow;
-    protected SlimeShadowStateMachine SlimeStateMachine => (SlimeShadowStateMachine)StateMachine;
-
-    public SlimeShadowState(Shadow shadow, ShadowStateMachine stateMachine) : base(shadow, stateMachine)
-    {
-    }
-}
