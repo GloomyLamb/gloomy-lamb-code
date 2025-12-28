5843ae78f7db3a3179b7f98a5fc024393b2d9e83
shin0112 <wnslcosltimo12@gmail.com>
2025-12-29 00:50:56 +0900

    remove: 불필요한 클래스, 코드 삭제
    - 그림자 개 상태 관련

--- 03_Game/02_Shadow/AnimationData/DogShadowAnimationData.cs
+++ 03_Game/02_Shadow/AnimationData/DogShadowAnimationData.cs
@@ -3,17 +3,14 @@ using UnityEngine;
 [System.Serializable]
 public class DogShadowAnimationData
 {
-    [SerializeField] private string _skillParameterName = "@Skill";
     [SerializeField] private string _biteParameterName = "Bite";
     [SerializeField] private string _barkParameterName = "Bark";
 
-    public int SkillParameterHash { get; private set; }
     public int BiteParameterHash { get; private set; }
     public int BarkParameterHash { get; private set; }
 
     public void Initialize()
     {
-        SkillParameterHash = Animator.StringToHash(_skillParameterName);
         BiteParameterHash = Animator.StringToHash(_biteParameterName);
         BarkParameterHash = Animator.StringToHash(_barkParameterName);
     }
