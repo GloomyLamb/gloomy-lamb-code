dd142c0cd3e3e1367fafbc2b4fa2c879c22dc535
shin0112 <wnslcosltimo12@gmail.com>
2025-12-29 17:41:14 +0900

    feat: 점액 밟기 사운드 넣기

--- 03_Game/02_Shadow/Skill/Mucus.cs
+++ 03_Game/02_Shadow/Skill/Mucus.cs
@@ -1,32 +1,13 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Mucus : MonoBehaviour
 {
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     private void OnTriggerEnter(Collider other)   // is Trigger 체크박스가 켜져 있어야 작동 트리거체크 
     {
-        if(other.gameObject.TryGetComponent<Player>(out Player player))
+        if (other.gameObject.TryGetComponent(out Player player))
         {
+            SoundManager.Instance.PlaySfxOnce(SfxName.Slime, idx: 1);
             player.TakeSlowDown();
         }
-    }
-
-    private void OnCollisionEnter(Collision collision)
-    {
-        
     }
 }
