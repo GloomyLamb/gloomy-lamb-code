ca1d585c8fb0c06865f6db1f1e7866bc59029662
shin0112 <wnslcosltimo12@gmail.com>
2025-12-30 17:28:13 +0900

    style: const 네이밍 컨벤션 수정

--- 10_UI/PopUpUI.cs
+++ 10_UI/PopUpUI.cs
@@ -6,7 +6,7 @@ public class PopUpUI : MonoBehaviour
     [SerializeField] private GameObject _popUpPrefab;
     private GameObject _popUpGo;
     private TextMeshProUGUI _popUpText;
-    private const string _defaultText = "현재 지원하지 않는 기능입니다.";
+    private const string DefaultText = "현재 지원하지 않는 기능입니다.";
 
     private void Init()
     {
@@ -22,7 +22,7 @@ public class PopUpUI : MonoBehaviour
             Init();
         }
         _popUpGo.SetActive(true);
-        _popUpText.text = string.IsNullOrEmpty(text) ? _defaultText : text;
+        _popUpText.text = string.IsNullOrEmpty(text) ? DefaultText : text;
     }
 
 #if UNITY_EDITOR
